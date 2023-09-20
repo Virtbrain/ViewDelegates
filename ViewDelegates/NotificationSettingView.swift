fb83bdf8662fc7a8b7e3867892f0af5381eb5d9d
AlexeyManankov <virtbrain@gmail.com>
2023-09-20 18:29:57 +0500

    new changes

--- ViewDelegates/NotificationSettingView.swift
+++ ViewDelegates/NotificationSettingView.swift
@@ -7,14 +7,63 @@
 
 import UIKit
 
+protocol NotificationSettingViewDelegate: AnyObject {
+    func didTapEnableButton()
+}
+
 class NotificationSettingView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    weak var delegate: NotificationSettingViewDelegate?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "bell")
+        imageView.tintColor = .systemBlue
+        return imageView
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "To stay up-to-date enable Push Notifications!"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Enable Notofocations", for: .normal)
+        button.backgroundColor = .systemGreen
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.setTitleColor(UIColor(white: 0.3, alpha: 1), for: .highlighted)
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(imageView)
+        addSubview(label)
+        addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let imageSize = bounds.size.width / 2
+        imageView.frame = CGRect(x: (bounds.size.width - imageSize) / 2, y: 30, width: imageSize, height: imageSize)
+        
+        label.frame = CGRect(x: 10, y: imageSize + 50, width: frame.size.width - 20, height: 100)
+        
+        button.frame = CGRect(x: 40, y: imageSize + 170, width: frame.size.width - 80, height: 50)
+    }
+    
+    @objc
+    private func didTapButton() {
+        delegate?.didTapEnableButton()
+    }
 }
--- ViewDelegates/ViewController.swift
+++ ViewDelegates/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let notificationsView = NotificationSettingView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(notificationsView)
+        notificationsView.delegate = self
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        notificationsView.frame = CGRect(
+            x: 10,
+            y: view.safeAreaInsets.top,
+            width: view.frame.size.width - 20,
+            height: view.frame.size.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom
+        )
+    }
 }
 
+extension ViewController: NotificationSettingViewDelegate {
+    func didTapEnableButton() {
+        let alert = UIAlertController(
+            title: "Enable Notifications",
+            message: "Some longer message here to tell user to do stuff",
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(UIAlertAction(title: "Dissmiss", style: .cancel, handler: nil))
+        
+        present(alert, animated: true)
+    }
+}
