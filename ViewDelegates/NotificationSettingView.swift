ef2a339fb958900478a194f4e7cd31f9bee53b8c
AlexeyManankov <virtbrain@gmail.com>
2023-09-20 18:29:17 +0500

    delegate is done

---
+++ ViewDelegates/NotificationSettingView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationSettingView.swift
+//  ViewDelegates
+//
+//  Created by Alexey Manankov on 20.09.2023.
+//
+
+import UIKit
+
+class NotificationSettingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
